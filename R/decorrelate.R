0a104ee2fbbe288b6d56c63659cf2b440967cb88
Gabriel Hoffman <gabrielhoffman@glia.local>
2021-03-12 22:41:47 -0500

    working version

--- R/decorrelate.R
+++ R/decorrelate.R
@@ -4,112 +4,181 @@
 # Linear time decorrelation projection using eclairs estimate of correlation 
 
 
+#' Multiply by eclairs matrix
+#'
+#' Multiply by eclairs matrix using special structure to achieve linear instead of cubic time complexity.
+#'
+#' @param X matrix to be transformed so *columns* are independent
+#' @param U1 orthonormal matrix with k columns representing the low rank component
+#' @param dSq1 eigen values so that U1 diag(dSq1) U1^T is the low rank component
+#' @param lambda shrinkage parameter for the convex combination.
+#' @param alpha exponent to be evaluated
 
+#' @details
+#' Let \eqn{\Sigma = U1 diag(dSq1) U1^T * (1-\lambda) + diag(\lambda, p)}, where \eqn{\lambda} shrinkage parameter for the convex combination between a low rank matrix and the identity matrix.
+#'
+#' Evaluate \eqn{X \Sigma^\alpha} in linear time using the special structure of the matrix
+#'
+mult_eclairs = function(X, U1, dSq1, lambda, alpha){
 
-#' @rdname decorrelate
-#' @export
-setGeneric("decorrelate_eclairs", function(x, cor.est) standardGeneric("decorrelate_eclairs"))
-
-
-#' @rdname decorrelate
-#' @export
-setMethod('decorrelate_eclairs', c(	x = "matrix", cor.est = "eclairs"), 
-	function(x, cor.est) {
-
-	decorrelate(x, cor.est$U, cor.est$dSq*(1- cor.est$lambda), cor.est$lambda)
-})
+	v = dSq1*(1-lambda) + lambda
+	X_U1 = X %*% U1
+ 	X_U1 %*% ((v^alpha) * t(U1))  + (X - tcrossprod(X_U1,U1) ) *(lambda^alpha)
+}
 
 
 
 #' Decorrelation projection
 #' 
-#' Decorrelation projection using low rank singular value decomposition of the correlation matrix
+#' Efficient decorrelation projection eclairs decomposition
 #'
-#' @param x matrix to be transformed so *columns* are independent
-#' @param U orthonormal matrix with k columns representing the low rank component
-#' @param dSq eigen values so that U diag(lamba) U^T is the low rank component
-#' @param sigSq residual variance used for the scaled diagonal component: diag(sigSq, p)
-#' @param cor.est estimate of correlation matrix from \link{eclairs} storing \code{U}, \code{dSq}, and \code{sigSq}
+#' @param X matrix to be transformed so *columns* are independent
+#' @param cor.est estimate of correlation matrix from \link{eclairs} storing \code{U}, \code{dSq}, and \code{lambda}
 #'
 #' @description
-#' Given a vector \eqn{x ~ N(0, \Sigma)} where \eqn{\Sigma = U diag(\lambda) U^T + diag(\sigma^2)}, transform x so that it has an identity covariance matrix.  \eqn{\Sigma^{-0.5}x} is such a projection (see Strimmer whitening).  When \eqn{\Sigma} is \eqn{p \times p}, computing this project naively is \eqn{O(p^3)}.  Here we take advantage of the fact that \eqn{\Sigma} is the sum of a low rank decomposition, plus a diagonal matrix with a single value.
+# FIX NOTATION HERE: Given a vector \eqn{x ~ N(0, \Sigma)} where \eqn{\Sigma = U diag(dSq1) U^T + diag(\sigma^2)}, transform x so that it has an identity covariance matrix.  \eqn{\Sigma^{-0.5}x} is such a projection (see Strimmer whitening).  When \eqn{\Sigma} is \eqn{p \times p}, computing this project naively is \eqn{O(p^3)}.  Here we take advantage of the fact that \eqn{\Sigma} is the sum of a low rank decomposition, plus a scaled identity matrix
 #'
-#' @rdname decorrelate
 #' @export
-setGeneric("decorrelate", function(x, U, dSq, sigSq) standardGeneric("decorrelate"))
-
-
-
-#' @rdname decorrelate
-#' @export
-setMethod('decorrelate', signature = c(
-				x 		= "matrix", 
-				U 		= "matrix", 
-				dSq 	= "numeric", 
-				sigSq	= "numeric"), 
-	function(x, U, dSq, sigSq) {
+decorrelate = function(X, cor.est){
 
 	transpose = FALSE
-	if( min(dim(x)) == 1 ){
+	if( min(dim(X)) == 1 ){
 		transpose = TRUE
-		x = matrix(x, nrow=1)
+		X = matrix(X, nrow=1)
 	}
 
-	# # add dimension checks here
-	if( ncol(x) != nrow(U) ){
-		stop("length of x must equal nrow(U)")
-	}
+	# alpha = -1/2 gives the decorrelating projection (i.e. whitening)
+	X.decorr = mult_eclairs(X, cor.est$U, cor.est$dSq, cor.est$lambda, alpha = -1/2)
 
-	if( ncol(U) != length(dSq) ){
-		stop("ncol(U) must equal length of dSq")
-	}
+	if( transpose ) X.decorr = t(X.decorr)
 
-	if( length(sigSq) != 1){
-		stop( "length of sigSq must be 1")
-	}
+	X.decorr
+}
 
-	x.decorr = lrmult(x, U, dSq, sigSq, alpha = -1/2)
 
-	if( transpose ) x.decorr = t(x.decorr)
 
-	x.decorr
-})
+
+
+
+
+
+
+# #' @rdname decorrelate
+# #' @export
+# setGeneric("decorrelate_eclairs", function(X, cor.est) standardGeneric("decorrelate_eclairs"))
+
+
+# #' @rdname decorrelate
+# #' @export
+# setMethod('decorrelate_eclairs', c(	X = "matrix", cor.est = "eclairs"), 
+# 	function(x, cor.est) {
+
+# 	decorrelate(X, cor.est$U, cor.est$dSq, cor.est$lambda)
+# })
+
+
+
+# #' Decorrelation projection
+# #' 
+# #' Decorrelation projection using low rank singular value decomposition of the correlation matrix
+# #'
+# #' @param X matrix to be transformed so *columns* are independent
+# #' @param U orthonormal matrix with k columns representing the low rank component
+# #' @param dSq eigen values so that U diag(lamba) U^T is the low rank component
+# #' @param lambda residual variance used for the scaled diagonal component: diag(sigSq, p)
+# #' @param cor.est estimate of correlation matrix from \link{eclairs} storing \code{U}, \code{dSq}, and \code{sigSq}
+# #'
+# #' @description
+# #' Given a vector \eqn{x ~ N(0, \Sigma)} where \eqn{\Sigma = U diag(\lambda) U^T + diag(\sigma^2)}, transform x so that it has an identity covariance matrix.  \eqn{\Sigma^{-0.5}x} is such a projection (see Strimmer whitening).  When \eqn{\Sigma} is \eqn{p \times p}, computing this project naively is \eqn{O(p^3)}.  Here we take advantage of the fact that \eqn{\Sigma} is the sum of a low rank decomposition, plus a diagonal matrix with a single value.
+# #'
+# #' @rdname decorrelate
+# #' @export
+# setGeneric("decorrelate", function(X, U, dSq, sigSq) standardGeneric("decorrelate"))
+
+
+
+# #' @rdname decorrelate
+# #' @export
+# setMethod('decorrelate', signature = c(
+# 				X 		= "matrix", 
+# 				U 		= "matrix", 
+# 				dSq 	= "numeric", 
+# 				sigSq	= "numeric"), 
+# 	function(x, U, dSq, sigSq) {
+
+# 	# transpose = FALSE
+# 	# if( min(dim(x)) == 1 ){
+# 	# 	transpose = TRUE
+# 	# 	x = matrix(x, nrow=1)
+# 	# }
+
+# 	# # add dimension checks here
+# 	if( ncol(x) != nrow(U) ){
+# 		stop("length of x must equal nrow(U)")
+# 	}
+
+# 	if( ncol(U) != length(dSq) ){
+# 		stop("ncol(U) must equal length of dSq")
+# 	}
+
+# 	if( length(sigSq) != 1){
+# 		stop( "length of sigSq must be 1")
+# 	}
+
+# 	x.decorr = mult_eclairs(X, U, dSq, sigSq, alpha = -1/2)
+
+# 	# if( transpose ) x.decorr = t(x.decorr)
+
+# 	x.decorr
+# })
+
+
+
+
+
+
+
+
+
+
+
+
 
 # low rank multiplication
 # x %*% (U diag(dSq) U^T + diag(sigSq))^alpha
-lrmult = function(x, U, dSq, sigSq, alpha){
+# lrmult = function(x, U, dSq, sigSq, alpha){
 
-	# precompute value that is used twice
-	a = (x - tcrossprod(x %*% U, U)) * sigSq^alpha
+# 	# precompute value that is used twice
+# 	a = (x - tcrossprod(x %*% U, U)) * sigSq^alpha
 
-	tcrossprod(x %*% U * (dSq + sigSq)^alpha, U) + (a - tcrossprod(a %*% U, U))
-}
-
-
-lrmult_t = function(x, U, dSq, sigSq, alpha){
-	lrmult(t(x), U, dSq, sigSq, alpha)
-}
+# 	tcrossprod(x %*% U * (dSq + sigSq)^alpha, U) + (a - tcrossprod(a %*% U, U))
+# }
 
 
+# lrmult_t = function(x, U, dSq, sigSq, alpha){
+# 	lrmult(t(x), U, dSq, sigSq, alpha)
+# }
 
-#  # low rank multiplication
-# # (U diag(dSq) U^T + diag(sigSq))^alpha %*% x
-lrmult_right = function(x, U, dSq, sigSq, alpha){
 
-	# precompute value that is used twice
-	a = (x - U %*% crossprod(U, x)) * sigSq^alpha
 
-	U %*% (crossprod(U, x) * (dSq + sigSq)^alpha) + (a - U %*% crossprod(U, a))
-}
+# #  # low rank multiplication
+# # # (U diag(dSq) U^T + diag(sigSq))^alpha %*% x
+# lrmult_right = function(x, U, dSq, sigSq, alpha){
 
-lrmult_left = function(x1, U, dSq, sigSq, alpha){
+# 	# precompute value that is used twice
+# 	a = (x - U %*% crossprod(U, x)) * sigSq^alpha
 
-	# precompute value that is used twice
-	b = (x1 - tcrossprod(x1 %*% U, U)) * sigSq^alpha
+# 	U %*% (crossprod(U, x) * (dSq + sigSq)^alpha) + (a - U %*% crossprod(U, a))
+# }
 
-	# (x1 %*% U * (dSq + sigSq)^alpha) %*% U + (b - tcrossprod(b,U) %*% U)
-	(x1 %*% U) %*% (( (dSq + sigSq)^alpha) * t(U)) + (b - tcrossprod(b %*% U, U))
-}
+# lrmult_left = function(x1, U, dSq, sigSq, alpha){
+
+# 	# precompute value that is used twice
+# 	b = (x1 - tcrossprod(x1 %*% U, U)) * sigSq^alpha
+
+# 	# (x1 %*% U * (dSq + sigSq)^alpha) %*% U + (b - tcrossprod(b,U) %*% U)
+# 	(x1 %*% U) %*% (( (dSq + sigSq)^alpha) * t(U)) + (b - tcrossprod(b %*% U, U))
+# }
 
 
 
@@ -165,62 +234,62 @@ lrmult_left = function(x1, U, dSq, sigSq, alpha){
 
 
 
-test_decorrelate = function(){
+# test_decorrelate = function(){
 
-	set.seed(1)
-	n = 20 # number of samples
-	p = 500 # number of features
-	k = 2 # rank of covariance matrix
-	lambda = 0.1
+# 	set.seed(1)
+# 	n = 20 # number of samples
+# 	p = 500 # number of features
+# 	k = 2 # rank of covariance matrix
+# 	lambda = 0.1
 
-	U_latent = svd(matrnorm(p,100))$u[,1:k, drop=FALSE]
-	Sigma = diag(.1, p) + tcrossprod(U_latent)
+# 	U_latent = svd(matrnorm(p,100))$u[,1:k, drop=FALSE]
+# 	Sigma = diag(.1, p) + tcrossprod(U_latent)
 
-	# sample matrix from MVN with covariance Sigma
-	Y = rmvnorm(n, rep(0, p), sigma=Sigma)
+# 	# sample matrix from MVN with covariance Sigma
+# 	Y = rmvnorm(n, rep(0, p), sigma=Sigma)
 
-	# sample vector from MVN with covariance Sigma
-	zstat = t(rmvnorm(1, rep(0, p), sigma=Sigma))
+# 	# sample vector from MVN with covariance Sigma
+# 	zstat = t(rmvnorm(1, rep(0, p), sigma=Sigma))
 
-	# Estimate covariance from Y
-	cor.est = eclairs( Y, k=3, lambda=lambda)
+# 	# Estimate covariance from Y
+# 	cor.est = eclairs( Y, k=3, lambda=lambda)
 
-	# perform low rank decorrelation projection
-	z.decorr = decorrelate((zstat), cor.est$U, cor.est$dSq*(1- cor.est$lambda), cor.est$lambda)
+# 	# perform low rank decorrelation projection
+# 	z.decorr = decorrelate(zstat, cor.est)
 
-	# perform *full* rank decorrelation projection
-	edcomp = eigen(Sigma)
+# 	# perform *full* rank decorrelation projection
+# 	edcomp = eigen(Sigma)
 
-	# Decorrelation using full spectrum from eigen decomp
-	M.transform = with(edcomp, vectors %*% diag(1/sqrt(values + lambda)) %*% t(vectors) + diag(1/sqrt(lambda), p))
-	z.transform = M.transform %*% zstat
+# 	# Decorrelation using full spectrum from eigen decomp
+# 	M.transform = with(edcomp, vectors %*% diag(1/sqrt(values + lambda)) %*% t(vectors) + diag(1/sqrt(lambda), p))
+# 	z.transform = M.transform %*% zstat
 
-	cor(z.transform, z.decorr)
-
-
-
-	cor.est = eclairs( Y, k=2, lambda)
-
-
-	z.decorr = decorrelate(zstat, cor.est$U, cor.est$dSq *(1-cor.est$lambda), cor.est$lambda)
-
-	cor(z.transform, z.decorr)
+# 	cor(z.transform, z.decorr)
 
 
 
-	# NO estimation here, just the linear algebra part
-	#######
-	dcmp = irlba::partial_eigen(Sigma, 20)
-	uhat = diag(Sigma - with(dcmp, vectors %*% diag(values) %*% t(vectors)))
+# 	cor.est = eclairs( Y, k=2, lambda)
 
-	# Compute residual variance of each feature
-	sigSq = mean(uhat)
 
-	z.decorr = decorrelate(zstat, dcmp$vectors, dcmp$values, sigSq)
+# 	z.decorr = decorrelate(zstat, cor.est)
 
-	cor(z.transform, z.decorr)
+# 	cor(z.transform, z.decorr)
+
+
+
+# 	# NO estimation here, just the linear algebra part
+# 	#######
+# 	dcmp = irlba::partial_eigen(Sigma, 1)
+# 	uhat = diag(Sigma - with(dcmp, vectors %*% diag(values) %*% t(vectors)))
+
+# 	# Compute residual variance of each feature
+# 	sigSq = mean(uhat)
+
+# 	z.decorr = decorrelate(zstat, dcmp$vectors, dcmp$values, sigSq)
+
+# 	cor(z.transform, z.decorr)
 	
-}
+# }
 
 
 
--- inst/unitTests/test_decorrelate.R
+++ inst/unitTests/test_decorrelate.R
@@ -34,4 +34,190 @@ test_decorrelate = function(){
 	checkIdentical(z.decorr, z.decorr2)
 }
 
-Y.decorr = decorrelate_eclairs(Y, cor.est )
+
+test_lrmult = function(){
+
+	suppressPackageStartupMessages({
+	library(Rfast)
+	})
+
+	set.seed(1)
+	n = 20 # number of samples
+	p = 500 # number of features
+	k = 2 # rank of covariance matrix
+	lambda = 0.1
+
+	U_latent = svd(matrnorm(p,100))$u[,1:k, drop=FALSE]
+	Sigma = diag(.1, p) + tcrossprod(U_latent)
+
+	# sample matrix from MVN with covariance Sigma
+	Y = rmvnorm(n, rep(0, p), sigma=Sigma)
+
+
+
+	dcmp = eigen(cov2cor(Sigma))
+	k = 10
+	U = dcmp$vectors
+	U1 = U[,1:k]
+	U2 = U[,-c(1:k)]
+	dcmp$values[-c(1:k)] = 0
+	dSq = dcmp$values
+	dSq1 = dSq[1:k]
+	lambda = 0.1
+
+	C1 = U %*% diag(dSq) %*% t(U) * (1-lambda) + diag(lambda, p)
+	# C2 = U %*% diag(dSq*(1-lambda)) %*% t(U) + diag(lambda, p)
+	# C2 = U %*% diag(dSq*(1-lambda) + lambda) %*% t(U) 
+	# C2 = U %*% diag(dSq*(1-lambda) + lambda) %*% t(U) 
+
+	v = dSq[1:k]*(1-lambda) + lambda
+	# C2 = U1 %*% diag(v) %*% t(U1) + U2 %*% diag(lambda, p-k) %*% t(U2)
+	# C2 = U1 %*% diag(v) %*% t(U1) + tcrossprod(U2) *lambda
+	# C2 = U1 %*% (v * t(U1)) + tcrossprod(U2) *lambda
+	C2 = U1 %*% (v * t(U1)) + tcrossprod(diag(1,p) - tcrossprod(U1)) *lambda
+
+	checkEquals(C1, C2)
+
+
+	# projection
+	alpha = -1/2
+	C = U %*% diag(dSq) %*% t(U) * (1-lambda) + diag(lambda, p)
+
+	decomp = eigen(C)
+	Y.decorr1 = Y %*% with(decomp, vectors %*% diag(values^alpha) %*% t(vectors))
+
+	# Y.decorr2 = Y %*% (U1 %*% (v^alpha * t(U1)) + tcrossprod(diag(1,p) - tcrossprod(U1)) *lambda^alpha)
+
+	# Y.decorr2 = (Y %*% U1) %*% (v^alpha * t(U1)) + Y %*%tcrossprod(diag(1,p) -  tcrossprod(U1)) *(lambda^alpha)
+
+	# Y.decorr2 = (Y %*% U1) %*% (v^alpha * t(U1)) +Y %*% (diag(1,p)  - tcrossprod(U1) ) *(lambda^alpha)
+
+	Y.decorr2 = (Y %*% U1) %*% (v^alpha * t(U1)) + (Y  - tcrossprod(Y %*% U1,U1) ) *(lambda^alpha)
+
+	checkEquals(Y.decorr1, Y.decorr2)
+
+
+	Y.decorr2 = mult_eclairs(Y, U1, dSq1, lambda, alpha)
+
+	checkEquals(Y.decorr1, Y.decorr2)
+}
+
+
+test_decorrelate_full_rank = function(){
+	# Check that decorrelate gives same value as 1/sqrt eigen values
+	######################################
+	set.seed(1)
+	n = 400 # number of samples
+	p = 500 # number of features
+	k = 2 # rank of covariance matrix
+	lambda = 0.1
+
+	# Create correlation matrix with autocorrelation
+	autocorr.mat <- function(p = 100, rho = 0.9) {
+	    mat <- diag(p)
+	    return(rho^abs(row(mat)-col(mat)))
+	}
+
+	Sigma = autocorr.mat(p, .9)
+	Sigma[1:3, 1:3]
+
+	# sample matrix from MVN with covariance Sigma
+	Y = rmvnorm(n, rep(0, p), sigma=Sigma)
+	# cor(Y[,1:3])
+
+
+	# decorrelate based on true correlation matrix
+	Y.decorr = Y %*% with(eigen(Sigma), vectors %*% diag(values^-0.5) %*% t(vectors))
+	# cor(Y.decorr[,1:3])
+
+	dcmp = eigen(Sigma)
+	cor.est = list(U = dcmp$vectors,
+				dSq = dcmp$values,
+				lambda =1e-10)
+
+	Y.decorr2 = decorrelate(Y, cor.est)
+
+	# Y.decorr2 = mult_eclairs(Y, cor.est$U, cor.est$dSq, cor.est$lambda, -1/2)
+	cor(Y.decorr2[,1:3])
+
+	checkEquals(Y.decorr, Y.decorr2)
+
+}
+
+test = function(){
+
+	# test statistical performance of decorrelate 
+	# 	It performs well with n >> p, but poorly in p >> n
+	#   However, this is not an issue because in regression, 
+	# 	the covariance matrix is actually known
+
+	library(Matrix)
+	set.seed(1)
+	n = 100 # number of samples
+	p = 2000 # number of features
+
+	# Create correlation matrix with autocorrelation
+	autocorr.mat <- function(p = 100, rho = 0.9) {
+	    mat <- diag(p)
+	    return(rho^abs(row(mat)-col(mat)))
+	}
+
+	Sigma = autocorr.mat(p/8, .9999)
+	Sigma = bdiag(Sigma, Sigma)
+	Sigma = bdiag(Sigma, Sigma)
+	Sigma = bdiag(Sigma, Sigma)
+
+	Sigma[1:3, 1:3]
+
+	# sample matrix from MVN with covariance Sigma
+	Y = rmvnorm(n, rep(0, p), sigma=Sigma)
+	# cor(Y[,1:3])
+
+	# decorrelated using true correlation matrix
+	Y.decorr.exact = Y %*% with(eigen(Sigma), vectors %*% diag(values^-0.5) %*% t(vectors))
+
+	cor(Y.decorr.exact[,1:3])
+	hist(cor(Y.decorr.exact))
+
+	ecl = eclairs(Y)
+
+	plot(ecl$dSq)
+
+	C = getCor(ecl)
+
+	Sigma[1:3, 1:3]
+	C[1:3, 1:3]
+
+
+	Y.decorr2 = decorrelate(Y, ecl)
+	cor(Y[,1:3])
+	cor(Y.decorr2[,1:3])
+
+
+
+	hist(cor(Y))
+	hist(cor(Y.decorr2))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
